cb38cc68e58308da204fcebc8893517d739aa7ae
Mary Lofton <melofton@vt.edu>
2023-11-21 15:26:05 -0500

    minor comments first 2 workflow scripts

--- source_scripts/00_package_downloads.R
+++ source_scripts/00_package_downloads.R
@@ -7,7 +7,8 @@ gc()
 if (!"pacman" %in% installed.packages()) install.packages("pacman")
 pacman::p_load(ggplot2, tidyverse, lubridate, readr, reshape2, zoo, patchwork,
                scales, gridExtra, maps, hexbin, rnaturalearth, sf, 
-               nlstools, minpack.lm, imputeTS, hydroGOF, devtools, vroom)
+               nlstools, minpack.lm, imputeTS, hydroGOF, devtools, vroom,
+               rnaturalearthdata)
 
 # Ryan, the following packages wouldn't load for me 
 # Failed to install/load:
@@ -22,3 +23,12 @@ pacman::p_load(ggplot2, tidyverse, lubridate, readr, reshape2, zoo, patchwork,
 # https://cran.r-project.org/doc/manuals/r-patched/R-admin.html#Installing-packages
 
 # Maybe we need to specify a version of R
+
+# but then when I went to load the packages, for sf and rnaturalearth I got
+# the following:
+# but this won't load
+# Error: package or namespace load failed for ‘rnaturalearth’ in dyn.load(file, DLLpath = DLLpath, ...):
+#   unable to load shared object '/usr/local/lib/R/site-library/units/libs/units.so':
+#   libudunits2.so.0: cannot open shared object file: No such file or directory
+
+# so now I am switching over to my laptop b/c I think this is a Docker problem
